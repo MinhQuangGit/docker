6a0eba08592b437125c65c22e1fd7b7ffec86358
DESKTOP-3T66CIS\Vu Minh Quang <vuminhquang1610198@gmail.com>
2023-06-22 14:37:43 +0700

    refactoring docker-compose.yaml

--- src/main/java/vn/com/mbbank/payment/mapper/Transaction2TransactionRespon.java
+++ src/main/java/vn/com/mbbank/payment/mapper/Transaction2TransactionRespon.java
@@ -7,8 +7,6 @@
 //import vn.com.mbbank.payment.model.TransactionRespon;
 //import vn.com.mbbank.payment.model.entity.Transaction;
 //
-//import java.util.Map;
-//
 //@Mapper
 //public interface Transaction2TransactionRespon extends  {
 //    Transaction2TransactionRespon INSTANCE = Map
